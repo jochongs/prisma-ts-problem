24ae50cd7f605337d6479fe0f1b6380812c55dd8
jochongs <joch2712@naver.com>
2025-02-09 16:47:29 +0900

    chore: Add case2

--- src/case2.ts
+++ src/case2.ts
@@ -6,7 +6,6 @@ const prisma = new PrismaClient();
 /**
  * id를 통해 게시글을 가져오는 메서드
  *
- * SelectUserWithPost를 리턴합니다.
  * (return type 에러가 발생 X)
  */
 async function getPostWithAuthorById(id: number): Promise<SelectUserWithPost> {
@@ -30,9 +29,6 @@ async function getPostWithAuthorById(id: number): Promise<SelectUserWithPost> {
     where: { id },
   });
 
-  // postWithAuthor.author.posts[0].title; // 빨간줄 X
-  // postWithAuthor.author.posts[0].id; // 빨간줄 O
-
   // ! id가 select되지 않아 posts: { title: string }[]로 평가되어도 return에서 빨간줄이 뜨지 않습니다.
   return postWithAuthor;
 }
@@ -40,16 +36,13 @@ async function getPostWithAuthorById(id: number): Promise<SelectUserWithPost> {
 /**
  * id를 통해 게시글을 가져오는 메서드
  *
- * SelectUserWithPostTypeShape를 리턴합니다.
  * (return type 에러가 발생 O)
  */
-async function getPostWithAuthorById2(
-  id: number
-): Promise<SelectUserWithPostTypeShape> {
+async function getPostWithAuthorById2(id: number): Promise<SelectUserWithPost> {
   const postWithAuthor = await prisma.post.findUniqueOrThrow({
     select: {
       id: true,
-      title: true,
+      // title: true, // <- 여기 있는 title은 select하지 않았을 때 에러가 발생
       author: {
         select: {
           id: true,
@@ -66,9 +59,6 @@ async function getPostWithAuthorById2(
     where: { id },
   });
 
-  // postWithAuthor.author.posts[0].title; // 빨간줄 X
-  // postWithAuthor.author.posts[0].id; // 빨간줄 O
-
-  // ! posts의 id가 select되지 않아 return에서 빨간줄이 발생합니다.
+  // ! title이 select 되지 않아 에러가 발생
   return postWithAuthor;
 }
