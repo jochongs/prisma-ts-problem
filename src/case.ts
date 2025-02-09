da9a72dd3a49fdb502017c5c3968b8e853af7c7f
jochongs <joch2712@naver.com>
2025-02-09 16:41:44 +0900

    chore: Update code comments in case.ts file

--- src/case.ts
+++ src/case.ts
@@ -7,6 +7,7 @@ const prisma = new PrismaClient();
  * id를 통해 게시글을 가져오는 메서드
  *
  * SelectUserWithPost를 리턴합니다.
+ * (return type 에러가 발생 X)
  */
 async function getPostWithAuthorById(id: number): Promise<SelectUserWithPost> {
   const postWithAuthor = await prisma.post.findUniqueOrThrow({
@@ -40,6 +41,7 @@ async function getPostWithAuthorById(id: number): Promise<SelectUserWithPost> {
  * id를 통해 게시글을 가져오는 메서드
  *
  * SelectUserWithPostTypeShape를 리턴합니다.
+ * (return type 에러가 발생 O)
  */
 async function getPostWithAuthorById2(
   id: number
