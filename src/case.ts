8f183b403195120f76fce2cfda488b3cc30acee5
jochongs <joch2712@naver.com>
2025-02-09 16:38:51 +0900

    chore: Add sample cases

---
+++ src/case.ts
@@ -0,0 +1,72 @@
+import { PrismaClient } from "@prisma/client";
+import { SelectUserWithPost, SelectUserWithPostTypeShape } from "./type";
+
+const prisma = new PrismaClient();
+
+/**
+ * id를 통해 게시글을 가져오는 메서드
+ *
+ * SelectUserWithPost를 리턴합니다.
+ */
+async function getPostWithAuthorById(id: number): Promise<SelectUserWithPost> {
+  const postWithAuthor = await prisma.post.findUniqueOrThrow({
+    select: {
+      id: true,
+      title: true,
+      author: {
+        select: {
+          id: true,
+          name: true,
+          posts: {
+            select: {
+              // id: true, // <- id는 select하지 않음
+              title: true,
+            },
+          },
+        },
+      },
+    },
+    where: { id },
+  });
+
+  postWithAuthor.author.posts[0].title; // 빨간줄 X
+  postWithAuthor.author.posts[0].id; // 빨간줄 O
+
+  // ! id가 select되지 않아 posts: { title: string }[]로 평가되어도 return에서 빨간줄이 뜨지 않습니다.
+  return postWithAuthor;
+}
+
+/**
+ * id를 통해 게시글을 가져오는 메서드
+ *
+ * SelectUserWithPostTypeShape를 리턴합니다.
+ */
+async function getPostWithAuthorById2(
+  id: number
+): Promise<SelectUserWithPostTypeShape> {
+  const postWithAuthor = await prisma.post.findUniqueOrThrow({
+    select: {
+      id: true,
+      title: true,
+      author: {
+        select: {
+          id: true,
+          name: true,
+          posts: {
+            select: {
+              // id: true, // <- id는 select하지 않음
+              title: true,
+            },
+          },
+        },
+      },
+    },
+    where: { id },
+  });
+
+  postWithAuthor.author.posts[0].title; // 빨간줄 X
+  postWithAuthor.author.posts[0].id; // 빨간줄 O
+
+  // ! posts의 id가 select되지 않아 return에서 빨간줄이 발생합니다.
+  return postWithAuthor;
+}
